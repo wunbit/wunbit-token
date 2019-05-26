aff770fce424d27b6ae317af20fc8563430ecb47
Carlos Oporto <carlosoporto@gmail.com>
2019-05-26 01:13:24 -0500

    transfer tokens

--- contracts/WunbitToken.sol
+++ contracts/WunbitToken.sol
@@ -7,10 +7,29 @@ contract WunbitToken {
   string public standard = "Wunbit Token v1.0";
   uint256 public totalSupply;
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+
   mapping(address => uint256) public balanceOf;
 
   constructor (uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
+  }
+
+  // Transfer function
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    // Transfer the balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+
   }
 }
