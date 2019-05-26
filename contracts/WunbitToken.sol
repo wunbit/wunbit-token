c00634bfed32ac144a55e3a202cdefb8ec58828c
Carlos Oporto <carlosoporto@gmail.com>
2019-05-26 16:38:45 -0500

    added approve function

--- contracts/WunbitToken.sol
+++ contracts/WunbitToken.sol
@@ -13,23 +13,40 @@ contract WunbitToken {
     uint256 _value
   );
 
+  // approve
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor (uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
   }
 
-  // Transfer function
   function transfer(address _to, uint256 _value) public returns (bool success) {
     require(balanceOf[msg.sender] >= _value);
-    // Transfer the balance
+
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
 
     emit Transfer(msg.sender, _to, _value);
 
     return true;
-
   }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  // transferFrom function
+
 }
